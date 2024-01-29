033c5222ad1a74f81be01c3eee4d81c171c5dcd4
Darshanum-27 <darshanum27@gmail.com>
2024-01-29 13:03:26 -0600

    Categorize by ZipCode

--- txt_to_Csv.py
+++ txt_to_Csv.py
@@ -12,3 +12,28 @@ with open(input_file_path, 'r', encoding='utf-8', errors='replace') as text_file
     for line in text_file:
         # Split the line by pipe and write to the CSV file
         writer.writerow(line.strip().split('|'))
+
+#   categorizing by Zip Codes and Displaying the total number of unique zipcodes generated
+categorizeByZip = dict()
+csvreader = csv.reader(open(output_file_path))
+header = next(csvreader)
+for row in csvreader:
+        if(row[9]!= None):
+            categorizeByZip[row[9]] = categorizeByZip.get(row[9],[])
+print(len(categorizeByZip))
+
+# categorizing by Zip Codes and Displaying the Number of records per unique zipcode
+csvreader = csv.reader(open(output_file_path))
+header = next(csvreader)
+for row in csvreader:
+    datavalue = categorizeByZip[row[9]]
+    if(datavalue!= None):
+        datavalue.append(row)
+        categorizeByZip[row[9]] = datavalue
+    
+for i in categorizeByZip.keys():
+    if(categorizeByZip[i] != None) and (i != None):
+        if(i == '     '):
+            print("ZIP Code Not Mentioned "+" has "+str(len(categorizeByZip[i]))+" locations")
+        else:
+            print(i+" has "+str(len(categorizeByZip[i]))+" locations")
